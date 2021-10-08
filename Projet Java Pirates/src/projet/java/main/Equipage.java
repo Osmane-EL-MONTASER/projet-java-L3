c6d2f0d1f171f7d27910bbd3320c037bdb7e99db
Osmane-cpp <63423818+Osmane-EL-MONTASER@users.noreply.github.com>
2021-10-08 21:07:13 +0200

    Ma partie avec le cout naif

--- Projet Java Pirates/src/projet/java/main/Equipage.java
+++ Projet Java Pirates/src/projet/java/main/Equipage.java
@@ -8,7 +8,6 @@ public class Equipage {
 	public static ArrayList<Tresor> tresors = new ArrayList<>();
 	public static Scanner sc = new Scanner(System.in);
 	
-	//TO DO Paul
 	public static void echanger(Pirate p1, Pirate p2) {
 		Tresor objP1,objP2;
 		objP1=p1.getObjetRecu();
@@ -17,23 +16,23 @@ public class Equipage {
 		p2.setObjetRecu(objP1);
 	}
 	
-	//TO DO Osmane
 	public static int calculerCoutNaif() {
 		int cout = 0;
 		
 		//Pour chaque pirate on ajoute le cout correpondant 
 		//au cout total.
 		for (Pirate p : pirates) {
-			
+			//Le pirate est jaloux donc on incrémente
+			if (p.estJaloux(pirates))
+				cout++;
 		}
 		
 		return cout;
 	}
 	
-	//TO DO Paul
 	public static void afficherCout(boolean isNaive) {
 		if(isNaive) {
-			System.out.println("Le coût est "+calculerCoutNaif());
+			System.out.println("Le coût naïf est "+calculerCoutNaif());
 		}
 	}
 	
@@ -42,9 +41,23 @@ public class Equipage {
 		
 	}
 	
-	//TO DO Osmane
 	public static void attributionNaive() {
+		ArrayList<Tresor> tresorsDispos = new ArrayList<Tresor>(tresors);
 		
+		//Pour chaque pirate on va attribuer
+		//son dernier tresor prefere disponible
+		for (Pirate p : pirates) {
+			boolean isTresorRecu = false;
+			char a = 'A';
+			for (int i = 0; i < p.getPreferences().size() && !isTresorRecu; i++) {
+				//Le tresor est disponible
+				if (tresorsDispos.contains(p.getPreferences().get(i))) {
+					p.setObjetRecu(p.getPreferences().get(i));
+					tresorsDispos.remove(p.getPreferences().get(i));
+					isTresorRecu = true;
+				}
+			}
+		}
 	}
 	
 	//TO DO Paul
@@ -60,5 +73,36 @@ public class Equipage {
 	
 	public static void main(String[] args) {
 		sc.close();
+		
+		tresors.add(new Tresor());
+		tresors.add(new Tresor());
+		tresors.add(new Tresor());
+		
+		pirates.add(new Pirate(3));
+		pirates.add(new Pirate(3));
+		pirates.add(new Pirate(3));
+		
+		pirates.get(0).setPreference(0, tresors.get(0));
+		pirates.get(0).setPreference(1, tresors.get(2));
+		pirates.get(0).setPreference(2, tresors.get(1));
+		pirates.get(1).setPreference(0, tresors.get(0));
+		pirates.get(1).setPreference(1, tresors.get(2));
+		pirates.get(1).setPreference(2, tresors.get(1));
+		pirates.get(2).setPreference(0, tresors.get(0));
+		pirates.get(2).setPreference(1, tresors.get(2));
+		pirates.get(2).setPreference(2, tresors.get(1));
+		
+		pirates.get(0).setRelation(0, true);
+		pirates.get(0).setRelation(1, true);
+		pirates.get(0).setRelation(2, true);
+		pirates.get(1).setRelation(0, true);
+		pirates.get(1).setRelation(1, false);
+		pirates.get(1).setRelation(2, true);
+		pirates.get(2).setRelation(0, false);
+		pirates.get(2).setRelation(1, true);
+		pirates.get(2).setRelation(2, false);
+		
+		attributionNaive();
+		afficherCout(true);
 	}
 }
--- Projet Java Pirates/src/projet/java/main/Pirate.java
+++ Projet Java Pirates/src/projet/java/main/Pirate.java
@@ -15,15 +15,21 @@ public class Pirate {
 			System.out.println("Impossible de nommer le pirate");
 		else {
 			nom=n++;
-			relations = new ArrayList<>(totalPirates);
-			preferences = new ArrayList<>(totalPirates);
+			
+			relations = new ArrayList<>();
+			preferences = new ArrayList<>();
+			
+			for (int i = 0; i < totalPirates; i++)
+				relations.add(false);
+			for (int i = 0; i < totalPirates; i++)
+				preferences.add(null);
 		}
 		
 	}
 	
-	//TO DO Osmane
 	public boolean relationAvec(Pirate p) {
-		return true;
+		int indexOfPirate = p.getNom() - 'A';
+		return relations.get(indexOfPirate);
 	}
 	
 	public boolean estJaloux(ArrayList<Pirate> pirates) {
@@ -34,8 +40,26 @@ public class Pirate {
 		if (preferences.get(0) == objetRecu)
 			return isJaloux;
 		
-		for (Pirate p : pirates) {
-			
+		//On prend les objets qu'il aurait 
+		//prefere avoir plutot que celui qu'il
+		//a recu
+		ArrayList<Tresor> meilleurPrefs = new ArrayList<Tresor>(preferences.subList(0, 
+				Integer.parseInt(objetRecu.toString().substring(1))));
+		
+		//On regarde si un pirate l'a recu
+		//et s'il a de mauvaises relations
+		//avec lui
+		for (int i = 0; i < pirates.size() && !isJaloux; i++) {
+			//Ce pirate detient un objet qu'il
+			//aurait prefere recevoir avant
+			//celui qu'il a recu
+			if (meilleurPrefs.contains(pirates.get(i).getObjetRecu())) {
+				//Le pirate est en effet dans
+				//de mauvaises relations avec
+				//ce pirate
+				if (relationAvec(pirates.get(i)))
+					isJaloux = true;
+			}
 		}
 		
 		return isJaloux;
@@ -60,12 +84,10 @@ public class Pirate {
 		this.objetRecu = objetRecu;
 	}
 	
-	//TO DO Paul
 	public void setRelation(int i, boolean b) {
 		relations.set(i, b);
 	}
 	
-	//TO DO Paul
 	public void setPreference(int i, Tresor t) {
 		preferences.set(i, t);
 	}
