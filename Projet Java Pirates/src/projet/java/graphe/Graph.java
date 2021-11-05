e25c2ddc4d07b789551f6c694ffd034416f2ece8
Osmane-cpp <63423818+Osmane-EL-MONTASER@users.noreply.github.com>
2021-11-05 20:05:15 +0100

    Graph extends

---
+++ Projet Java Pirates/src/projet/java/graphe/Graph.java
@@ -0,0 +1,50 @@
+package projet.java.graphe;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+public class Graph<T> {
+	//Une matrice d'adjacence.
+	protected LinkedHashMap<Vertex<T, T>, Boolean> adjacency;
+	protected int nbT;
+	
+	public Graph(int nbT, ArrayList<T> Ts) {
+		this.nbT = nbT;
+		
+		adjacency = new LinkedHashMap<>();
+		
+		for (int i = 0; i < nbT; i++)
+			for (int j = 0; j < nbT; j++)
+				adjacency.put(new Vertex<T, T>(Ts.get(i), Ts.get(j)), false);
+	}
+	
+	public void changeRelation(T p1, T p2, boolean r) {
+		adjacency.replace(new Vertex<T, T>(p1, p2), r);
+	}
+	
+	public boolean getRelation(T p1, T p2) {
+		return adjacency.get(new Vertex<T, T>(p1, p2));
+	}
+	
+	@Override
+	public String toString() {
+		String str = new String();
+		int i = 0;
+		
+		str += "Matrice d'adjacence : \n";
+		for (Entry<Vertex<T, T>, Boolean> e : adjacency.entrySet()) {
+			if (i == nbT) {
+				str += "\n";
+				i = 0;
+			}
+			
+			str += e.getValue() + "\t";
+			i++;
+		}
+		
+		str += "\n";
+		
+		return str;
+	}
+}
--- Projet Java Pirates/src/projet/java/graphe/GraphePirate.java
+++ Projet Java Pirates/src/projet/java/graphe/GraphePirate.java
@@ -6,27 +6,10 @@ import java.util.Map.Entry;
 
 import projet.java.main.Pirate;
 
-public class GraphePirate {
-	//Une matrice d'adjacence.
-	private LinkedHashMap<Vertex<Pirate, Pirate>, Boolean> adjacency;
-	private int nbPirate;
+public class GraphePirate extends Graph {
 	
 	public GraphePirate(int nbPirate, ArrayList<Pirate> pirates) {
-		this.nbPirate = nbPirate;
-		
-		adjacency = new LinkedHashMap<>();
-		
-		for (int i = 0; i < nbPirate; i++)
-			for (int j = 0; j < nbPirate; j++)
-				adjacency.put(new Vertex<Pirate, Pirate>(pirates.get(i), pirates.get(j)), false);
-	}
-	
-	public void changeRelation(Pirate p1, Pirate p2, boolean r) {
-		adjacency.replace(new Vertex<Pirate, Pirate>(p1, p2), r);
-	}
-	
-	public boolean getRelation(Pirate p1, Pirate p2) {
-		return adjacency.get(new Vertex<Pirate, Pirate>(p1, p2));
+		super(nbPirate, pirates);
 	}
 	
 	@Override
@@ -34,9 +17,11 @@ public class GraphePirate {
 		String str = new String();
 		int i = 0;
 		
+		LinkedHashMap<Vertex<Pirate, Pirate>, Boolean> temp = (LinkedHashMap<Vertex<Pirate, Pirate>, Boolean>) adjacency;
+		
 		str += "Matrice d'adjacence de l'équipage : \n";
-		for (Entry<Vertex<Pirate, Pirate>, Boolean> e : adjacency.entrySet()) {
-			if (i == nbPirate) {
+		for (Entry<Vertex<Pirate, Pirate>, Boolean> e : temp.entrySet()) {
+			if (i == nbT) {
 				str += "\n";
 				i = 0;
 			}
