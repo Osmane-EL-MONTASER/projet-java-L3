44686a3ff74f6e9dadb02a0901ecd79dd21e7386
Osmane-cpp <63423818+Osmane-EL-MONTASER@users.noreply.github.com>
2021-12-17 18:37:07 +0100

    Doc

--- Projet Java Pirates Console Version/doc/package-search-index.js
+++ Projet Java Pirates Console Version/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"project.crew.sharing"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"project"},{"l":"project.crew"},{"l":"project.crew.graph"},{"l":"project.crew.io"},{"l":"project.crew.sharing"},{"l":"project.graph"},{"l":"project.graph.exceptions"}]
